33e4fcc5b652cbcec67a29c2359790282ab9a2d6
arthursilveiradev <arthurmdsilveira2004@gmail.com>
2026-09-23 11:55:09 -0300

    feat: Change infrastructure

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = "rg-example-terraform"
   location = "brazilsouth"
 
   tags = {
     Name = "Hello World"
+    Environment = "Terraform Getting Started"
+    Team = "DevOps"
   }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "vnet-example"
+  location = "brazilsouth"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space = ["10.0.0.0/16"]
 }
